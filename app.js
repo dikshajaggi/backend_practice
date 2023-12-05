19c32ad71a3d1e4fbf956e093e075e2528c64251
diksha <diksha2000may@gmail.com>
2023-12-05 19:56:56 +0530

    user apis created

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import addProductRoutes from './apis/AddProduct.api.js'
 import addCategoryRoutes from './apis/AddCategory.api.js'
 import authRoutes from "./apis/Auth.api.js"
 import categoryRoute from "./apis/Category.api.js"
+import userRoutes from "./apis/User.api.js"
 
 const { json } = pkg;
 const app = express();
@@ -18,6 +19,7 @@ app.use(json());
 
 // Connect to MongoDB
 connectDB();
+app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', authenticateAdmin, addProductRoutes, addCategoryRoutes);
 app.use('/api/', productRoutes, categoryRoute);
---
+++ models/user.models.js
@@ -0,0 +1,12 @@
+// models/user.model.js
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
